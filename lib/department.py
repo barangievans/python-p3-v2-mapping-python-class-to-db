961cea3f70433576208ddd33311e289d32f5d6c2
barangievans <barangievans@gmail.com>
2024-09-20 15:51:43 +0300

    mapping

--- lib/department.py
+++ lib/department.py
@@ -1,6 +1,9 @@
 from __init__ import CURSOR, CONN
 
 class Department:
+    # Dictionary to store cached instances
+    all = {}
+
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
@@ -31,12 +34,48 @@ class Department:
         CURSOR.execute(sql, (self.name, self.location))
         CONN.commit()
         self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self  # Cache the instance
 
     @classmethod
     def create(cls, name, location):
+        """Initialize a new Department instance and save the object to the database."""
         department = cls(name, location)
         department.save()
         return department
+
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return a Department object having the attribute values from the table row."""
+        department = cls.all.get(row[0])
+        if department:
+            department.name = row[1]
+            department.location = row[2]
+        else:
+            department = cls(row[1], row[2])
+            department.id = row[0]
+            cls.all[department.id] = department
+        return department
+
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a Department object per row in the table."""
+        sql = "SELECT * FROM departments"
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_id(cls, id):
+        """Return a Department object corresponding to the table row matching the specified primary key."""
+        sql = "SELECT * FROM departments WHERE id = ?"
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    @classmethod
+    def find_by_name(cls, name):
+        """Return a Department object corresponding to first table row matching specified name."""
+        sql = "SELECT * FROM departments WHERE name = ?"
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
     def update(self):
         sql = "UPDATE departments SET name = ?, location = ? WHERE id = ?"
@@ -44,6 +83,9 @@ class Department:
         CONN.commit()
 
     def delete(self):
+        """Delete the table row corresponding to the current Department instance."""
         sql = "DELETE FROM departments WHERE id = ?"
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
+        del type(self).all[self.id]  # Remove from cache
+        self.id = None
